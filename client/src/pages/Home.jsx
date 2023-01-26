23246ea90c81d7d1f6636c94fd2779e28f7906f0
Alan Xu <alan.xu1699@gmail.com>
2023-01-26 13:09:59 -0500

    import Card, FormField, and Loader into Home.jsx page, create useStates for allposts and loading

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,22 @@
 import React, {useState, useEffect} from "react";
+import {Card, FormField, Loader} from '../components'
 
 const Home = () => {
+
+  const [loading, setLoading] = useState(false);
+  const [allPosts, setAllPosts] = useState(null);
+
   return (
-    <div>Home</div>
+    <section className="max-w-7xl mx-auto">
+      <div>
+        <h1 className="font-extrabold text-[#222328]">The Community Showcase</h1>
+        <p className="mt-2 text-[#666e75] text-[16px] max-w[500px]">Introducing new profound facial AI program that allows users the ability to fetch any randomly generated images.</p>
+      </div>
+
+      <div className="mt-16">
+        <FormField/>
+      </div>
+    </section>
   )
 };
 
