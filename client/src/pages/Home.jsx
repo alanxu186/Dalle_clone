90c9aec8074908a7088f51c4225cb7ac637df9ce
Alan Xu <alan.xu1699@gmail.com>
2023-01-26 13:20:33 -0500

    create useState for searchText, include ternary operation to render searched text in an span tag

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
-import React, {useState, useEffect} from "react";
-import {Card, FormField, Loader} from '../components'
+import React, { useState, useEffect } from "react";
+import { Card, FormField, Loader } from '../components'
 
 const Home = () => {
 
   const [loading, setLoading] = useState(false);
   const [allPosts, setAllPosts] = useState(null);
+
+  const [searchText, setSearchText] = useState('hello world');
 
   return (
     <section className="max-w-7xl mx-auto">
@@ -14,7 +16,23 @@ const Home = () => {
       </div>
 
       <div className="mt-16">
-        <FormField/>
+        <FormField />
+      </div>
+
+      <div className="mt-10">
+        {loading ? (
+          <div className="flex justify-center items-center">
+            <Loader />
+          </div>
+        ) : (
+          <>
+            {searchText && (
+              <h2 className="font-medium text-[#666e75] text-xl mb-3">
+                Showing results for <span className="text-[#222328]">{searchText}</span>
+              </h2>
+            )}
+          </>
+        )}
       </div>
     </section>
   )
