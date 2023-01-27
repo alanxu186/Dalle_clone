b10901fbba99e850484194ef79c39155a7c1ecc5
Alan Xu <alan.xu1699@gmail.com>
2023-01-26 22:18:21 -0500

    initiate useNavigate, declare useStates for form, generatingImg, loading, create handleSubmit function for form, create form element within section

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { preview } from "../assets";
@@ -6,8 +6,30 @@ import { getRandomPrompt } from '../utils/index';
 import { FormField, Loader } from "../components";
 
 const CreatePost = () => {
+  const navigate = useNavigate();
+  const [form, setForm] = useState({
+    name: '',
+    prompt: '',
+    photo: '',
+  });
+  const [generatingImg, setGeneratingImg] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = () => {
+    
+  }
+
   return (
-    <div>Create Post</div>
+    <section className="max-w-7xl mx-auto">
+      <div>
+        <h1 className="font-extrabold text-[#222328]">Create</h1>
+        <p className="mt-2 text-[#666e75] text-[16px] max-w[500px]">Make some great things and share with the community</p>
+      </div>
+
+      <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
+
+      </form>
+    </section>
   )
 };
 
