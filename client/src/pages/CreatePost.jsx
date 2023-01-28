081fedbf710d329ecb1cc95cd25dc80ecdd7f41d
Alan Xu <alan.xu1699@gmail.com>
2023-01-28 17:00:34 -0500

    setForm state to reflect target value of name in handleChange function, call getRandomPrompt to generate random prompts in handleSurpriseMe function

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -24,11 +24,12 @@ const CreatePost = () => {
   }
 
   const handleChange = (e) => {
-
+    setForm({ ...form, [e.target.name]: e.target.value })
   }
 
   const handleSurpriseMe = () => {
-
+    const randomPrompt = getRandomPrompt(form.prompt);
+    setForm({ ...form, prompt: randomPrompt })
   }
 
   return (
@@ -80,13 +81,25 @@ const CreatePost = () => {
         </div>
 
         <div className="mt-5 flex gap-5">
-            <button
+          <button
             type="button"
             onClick={generateImage}
             className="text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
-            >
-              {generatingImg ? 'Generating...' : 'Generate'}
-            </button>
+          >
+            {generatingImg ? 'Generating...' : 'Generate'}
+          </button>
+        </div>
+
+        <div className="mt-10">
+          <p className="mt-2 text-[#666e75] text-[14px]">
+            Once you have created the image you want, you can share it with others in the community
+          </p>
+          <button
+            type="submit"
+            className="mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+          >
+            {loading ? 'Sharing' : 'Share with the community'}
+          </button>
         </div>
       </form>
     </section>
