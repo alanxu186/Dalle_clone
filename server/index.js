f814ef91a31a63becd491dc7dc3fa954d186bb3f
Alan Xu <alan.xu1699@gmail.com>
2023-01-29 12:14:51 -0500

    test gitignore

--- server/index.js
+++ server/index.js
@@ -2,20 +2,29 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 
+import connectDB from './mongodb/connect.js';
+
 dotenv.config();
 
 const app = express()
 //middleware
 app.use(cors());
-app.use(express.json({limit: '50mb'}));
+app.use(express.json({ limit: '50mb' }));
 
 //routes
-app.get('/', async (req,res) => {
+app.get('/', async (req, res) => {
     res.send('Hello from DALL-E!');
 });
 
 const startServer = async () => {
-    app.listen(8080, () => console.log('Server has started on port http://localhost:8080'))
+
+    try {
+        connectDB(process.env.MONGODB_URL);
+        app.listen(8080, () => console.log('Server has started on port http://localhost:8080'))
+    } catch (error) {
+        console.log(error);
+    }
+
 }
 
 startServer();
