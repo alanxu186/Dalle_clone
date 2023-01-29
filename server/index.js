36bc2746d98685e38ec68de4189a7e2b7cf87171
Alan Xu <alan.xu1699@gmail.com>
2023-01-29 17:07:42 -0500

    create file path for dalleRoutes and postRoutes in index.js, import express, dotenv, and v2 from cloudinary in postRoutes.js, import express, dotenv, configuration and openaiapi in dalleRoutes.js

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ import * as dotenv from 'dotenv';
 import cors from 'cors';
 
 import connectDB from './mongodb/connect.js';
+import dalleRoutes from './routes/dalleRoutes';
+import postRoutes from './routes/postRoutes';
 
 dotenv.config();
 
@@ -10,6 +12,9 @@ const app = express()
 //middleware
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
+
+app.use('/api/v1/dalle', dalleRoutes);
+app.use('/api/v1/posts', postRoutes);
 
 //routes
 app.get('/', async (req, res) => {
