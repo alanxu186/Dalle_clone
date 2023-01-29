6b692e4e423128c320a97dc45629c0b0c13f6205
Alan Xu <alan.xu1699@gmail.com>
2023-01-29 11:42:06 -0500

    call cors and express.json middlewares, create GET route to test if server is running, run server and listen for it on port 8080

--- server/index.js
+++ server/index.js
@@ -5,3 +5,17 @@ import cors from 'cors';
 dotenv.config();
 
 const app = express()
+//middleware
+app.use(cors());
+app.use(express.json({limit: '50mb'}));
+
+//routes
+app.get('/', async (req,res) => {
+    res.send('Hello from DALL-E!');
+});
+
+const startServer = async () => {
+    app.listen(8080, () => console.log('Server has started on port http://localhost:8080'))
+}
+
+startServer();
