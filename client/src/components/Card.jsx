d7004eed0077902472d38c28b561ba230588ad8c
Alan Xu <alan.xu1699@gmail.com>
2023-02-04 19:50:15 -0500

    import file-saver in index.js file in utils folder

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React from 'react';
+
+import { download } from '../assets';
+import { downloadImage } from '../utils';
 
 const Card = () => {
     return (
--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,5 +1,7 @@
 import { surpriseMePrompts } from '../constants';
 
+import FileSaver from 'file-saver';
+
 // Get random prompt for user to use to generate a random ai image
 export function getRandomPrompt(prompt) {
     const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
@@ -9,4 +11,11 @@ export function getRandomPrompt(prompt) {
     if(randomPrompt === prompt) return getRandomPrompt(prompt)
 
     return randomPrompt;
+}
+
+//import FileSaver
+//pass _id,photo parameters
+//implement function 
+export async function downloadImage(_id, photo) {
+    FileSaver.saveAs(photo, `download-${_id}.jpeg`);
 }
