6abc55fd8b428487d482a29b964125bfb9c82325
Alan Xu <alan.xu1699@gmail.com>
2023-02-05 00:10:39 -0500

    implement download image button, call downloadImage function in a callback function onClick

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -22,8 +22,10 @@ const Card = ({ _id, name, prompt, photo }) => {
                         </div>
                         <p className='text-white text-sm'>{name}</p>
                     </div>
+                    <button type='button' onClick={() => downloadImage(_id, photo)} className='outline-none bg-transparent border-none'>
+                        <img src={download} alt='download' className='w-6 h-6 object-contain invert'></img>
+                    </button>
                 </div>
-
 
             </div>
         </div>
