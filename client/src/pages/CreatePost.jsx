1919e5611ecf2f84b6ebf6108b99ae407f5d45bb
Alan Xu <alan.xu1699@gmail.com>
2023-02-05 16:50:16 -0500

    run npm install build, website deployed on netlify

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -48,7 +48,7 @@ const CreatePost = () => {
       setLoading(true);
 
       try {
-        const response = await fetch('http://localhost:8080/api/v1/post', {
+        const response = await fetch('https://dall-e-ol3r.onrender.com/api/v1/post', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
